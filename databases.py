90e21a9e38d70919b750671ce45c8a8bc72d5631
shiraz18-meet <shira.zuker1@gmail.com>
2017-02-09 19:59:47 +0200

    idk idk idk

--- databases.py
+++ databases.py
@@ -4,11 +4,11 @@ from sqlalchemy.orm import relationship, sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
 from flask import *
-
+from passlib.apps import custom_app_context as pwd_context
 Base = declarative_base()
 
 
-class books(Base):
+class Book(Base):
 	__tablename__ = 'books'
 	id = Column(Integer, primary_key=True)
 	title = Column(String)
@@ -18,7 +18,7 @@ class books(Base):
 	genre = Column(String)
 	location = Column(String)
 
-class users(Base):
+class User(Base):
 	__tablename__ = 'users'
 	id = Column(Integer, primary_key=True)
 	firstname = Column(String)
@@ -26,14 +26,14 @@ class users(Base):
 	email = Column(String, unique=True)
 	password = Column(String)
 	imgurluser = Column(String)
-	dob = Column(Date)
+	dob = Column(String)
 	phonenumber = Column(String)
 
-	def hash_password(self, password):
-		self.password_hash = pwd_context.encrypt(password)
+	# def hash_password(self, password):
+	# 	self.hash_password = pwd_context.encrypt(password)
 
-	def verify_password(self, password):
-		return pwd_context.verify(password, self.password_hash)
+	# def verify_password(self, password):
+	# 	return pwd_context.verify(password, self.hash_password)
 
 
 
--- webapp.py
+++ webapp.py
@@ -1,24 +1,38 @@
 from flask import Flask, url_for, flash, redirect, request, g
 from databases import *
 from flask import session as login_session
+from passlib.apps import custom_app_context as pwd_context
+
 
 app = Flask(__name__)
 app.secret_key = "MY_SUPER_SECRET_KEY"
 
+# def verify_password(email, password):
+# 	user = session.query(User).filter_by(email=email).first()
+# 	if not user or not user.verify_password(password):
+# 		return False
+# 	g.user = user
+# 	return True
+
 def verify_password(email, password):
-	user = session.query(user).filter_by(email=email).first()
-	if not user or not customer.verify_password(password):
-		return False
+	user = session.query(User).filter_by(email=email).first()
+	if pwd_context.encrypt(password) == user.password:
+		return True
 	return False
+
+def hash_password(password):
+	return pwd_context.encrypt(password)
+
 
 
 @app.route('/')
 def world():
 	return "hello world"
-@app.route('/inventory/')
+@app.route('/home')
 def home():
-	items = session.query(books).all()
-	return render_template('inventory.html', items = items)
+	items = session.query(Book).all()
+	print ("home")
+	return render_template('home.html', items = items)
 
 @app.route('/signup', methods = ['GET', 'POST'])
 def signup():
@@ -34,15 +48,14 @@ def signup():
 		if firstname == "" or lastname == "" or email == "" or password == "" or confirmpassword == "" or dob == "" or phonenumber == "":
 			flash("Your form is missing arguments")
 			return redirect(url_for('signup'))
-		if session.query(users).filter_by(email=email). first() is not None:
+		if session.query(User).filter_by(email=email). first() is not None:
 			flash("A user with this email address already exists")
 			return redirect(url_for('signup')) 
-		user = users(firstname=firstname,lastname=lastname, password=password, confirmpassword=confirmpassword, email=email, imgurluser=imgurluser, dob=dob, phonenumber=phonenumber)
-		user.hash_password(password)
+		user = User(firstname=firstname,lastname=lastname, password=hash_password(password), email=email, imgurluser=imgurluser, dob=dob, phonenumber=phonenumber)
 		session.add(user)
 		session.commit()
 		flash("User created successfully")
-		return redirect(url_for('inventory'))
+		return redirect( url_for('home'))
 	else:
 		return render_template('signup.html')
 
@@ -54,19 +67,20 @@ def login():
 	if request.method == 'GET':
 		return render_template('login.html')
 	elif request.method == 'POST':
-		email=request.form['email']
+		email = request.form['email']
 		password = request.form['password']
 		if email is None or password is None:
 			flash("Missing Arguements")
 			return redirect(url_for('login'))
-		if verify_password(email, password):
-			user = session.query(users).filter_by(email=email).one()
+		if verify_password(email, hash_password(password)):
+			user = session.query(User).filter_by(email=email).first()
 			flash('Login Successful. Welcome, %s' %user.firstname)
 			login_session['firstname']= user.firstname
 			login_session['lastname'] = user.lastname
 			login_session['email'] = user.email
+			print("LogIn")
 			login_session['id'] = user.id
-			return redirect(url_for('inventory'))
+			return redirect(url_for('home'))
 		else:
 			flash('Incorrect email/password combination')
 			return redirect(url_for('login'))
@@ -83,7 +97,7 @@ def newbook():
 		if title == "" or author == "" or pubyear == "" or genre == "" or location == "":
 			flash("Your form is missing arguements")
 			return redirect(url_for('newbook'))
-		book = book(title=title, author=author, pubyear=pubyear, imgurlbook=imgurlbook, genre=genre, location=location)
+		book = Book(title=title, author=author, pubyear=pubyear, imgurlbook=imgurlbook, genre=genre, location=location)
 		session.add(book)
 		session.commit()
 		flash("Book added successfully!")
@@ -95,7 +109,7 @@ def newbook():
 
 @app.route('/user/<email>')
 def user_profile(user_email):
-	user_email = session.query(users).filter_by(email).first(user_email=user_email)
+	user_email = session.query(User).filter_by(email).first(user_email=user_email)
 	return render_template('user.html', user_email=user_email)
 
 
@@ -103,8 +117,8 @@ def user_profile(user_email):
 
 @app.route('/book/<int:book_id>')
 def book (book_id):
-	book = session.query(books).filter_by(id=book_id).one()
-	return render_template('inventory.html', book=book)
+	book = session.query(Book).filter_by(id=book_id).one()
+	return render_template('ihome.html', book=book)
 
 
 
@@ -118,7 +132,7 @@ def logout():
 	del login_session['email']
 	del login_session['id']
 	flash("Logged out successfully")
-	return redirect(url_for('inventory'))
+	return redirect(url_for('home'))
 
 
 
